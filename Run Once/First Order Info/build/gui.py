f5d0e31fb52ec124c500bb8ef01e77f75b9f02e6
Venexs <51032291+Venexs@users.noreply.github.com>
2024-03-25 20:42:44 +0300

    Add files via upload

--- Run Once/First Order Info/build/gui.py
+++ Run Once/First Order Info/build/gui.py
@@ -12,7 +12,7 @@ import subprocess
 subprocess.Popen(['python', 'sfx.py'])
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Projects\System\Run Once\First Order Info\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"Run Once\First Order Info\build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
--- Run Once/Main Check/build/gui.py
+++ Run Once/Main Check/build/gui.py
@@ -12,7 +12,7 @@ import subprocess
 subprocess.Popen(['python', 'sfx.py'])
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Projects\System\System Panel Templates\Run Once\Main Check\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"System Panel Templates\Run Once\Main Check\build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
