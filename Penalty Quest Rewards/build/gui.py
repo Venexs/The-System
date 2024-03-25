119237e85ad44debf12ca04898e6dca8abbca348
Venexs <51032291+Venexs@users.noreply.github.com>
2024-03-25 20:40:17 +0300

    Add files via upload

--- Penalty Quest Rewards/build/gui.py
+++ Penalty Quest Rewards/build/gui.py
@@ -11,7 +11,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Projects\System\Penalty Quest Rewards\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"Penalty Quest Rewards\build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
