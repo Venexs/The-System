d41d8cc3fae430f5b5174922dd53b1a1daed97a6
Venexs <51032291+Venexs@users.noreply.github.com>
2024-03-25 20:34:21 +0300

    Add files via upload

--- AGI Point addition/build/gui.py
+++ AGI Point addition/build/gui.py
@@ -13,7 +13,7 @@ import subprocess
 subprocess.Popen(['python', 'sfx.py'])
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Projects\System\AGI Point addition\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"AGI Point addition\build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
