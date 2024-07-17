a50e91048e59d0819488c2455e7ad87d32070b83
Venexs <51032291+Venexs@users.noreply.github.com>
2024-07-17 12:55:03 +0300

    Ver 0.6 Update 5

--- Anime Version/Quests/build/gui.py
+++ Anime Version/Quests/build/gui.py
@@ -11,6 +11,8 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import csv
 import subprocess
 import json
+import cv2
+from PIL import Image, ImageTk
 
 subprocess.Popen(['python', 'sfx.py'])
 OUTPUT_PATH = Path(__file__).parent
@@ -26,6 +28,56 @@ window = Tk()
 window.geometry("555x729")
 window.configure(bg = "#FFFFFF")
 window.attributes('-alpha',0.8)
+window.overrideredirect(True)
+window.wm_attributes("-topmost", True)
+#window.update()
+
+class VideoPlayer:
+    def __init__(self, canvas, video_path, x, y):
+        self.canvas = canvas
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
+        self.x = x
+        self.y = y
+        self.image_id = self.canvas.create_image(self.x, self.y)
+        self.update_frame()
+
+    def update_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            # If the video has ended, reset the capture object
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret, frame = self.cap.read()
+
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.canvas.itemconfig(self.image_id, image=imgtk)
+            self.canvas.imgtk = imgtk
+
+        self.canvas.after(10, self.update_frame)
+
+    def __del__(self):
+        self.cap.release()
+
+def start_move(event):
+    global lastx, lasty
+    lastx = event.x_root
+    lasty = event.y_root
+
+def move_window(event):
+    global lastx, lasty
+    deltax = event.x_root - lastx
+    deltay = event.y_root - lasty
+    x = window.winfo_x() + deltax
+    y = window.winfo_y() + deltay
+    window.geometry("+%s+%s" % (x, y))
+    lastx = event.x_root
+    lasty = event.y_root
+
+def ex_close(win):
+    win.quit()
 
 canvas = Canvas(
     window,
@@ -45,6 +97,9 @@ image_1 = canvas.create_image(
     404.0,
     image=image_image_1
 )
+
+video_path = "Files/0001-0200.mp4"
+player = VideoPlayer(canvas, video_path, 277.0, 380.0)
 
 image_image_2 = PhotoImage(
     file=relative_to_assets("image_2.png"))
@@ -677,5 +732,33 @@ button_13.place(
     width=24.0,
     height=24.0
 )
+
+image_0=canvas.create_rectangle(
+    0.0,
+    0.0,
+    570.0,
+    35.0,
+    fill="#212121",
+    outline="")
+
+canvas.tag_bind(image_0, "<ButtonPress-1>", start_move)
+canvas.tag_bind(image_0, "<B1-Motion>", move_window)
+
+button_image_0 = PhotoImage(
+    file=relative_to_assets("button_0.png"))
+button_0 = Button(
+    image=button_image_0,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: ex_close(window),
+    relief="flat"
+)
+button_0.place(
+    x=520.0,
+    y=3.0,
+    width=28.0,
+    height=28.0
+)
+
 window.resizable(False, False)
 window.mainloop()
